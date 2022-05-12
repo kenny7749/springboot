931b5834ab5e0e7cd5c998e678861dad9b8fe5d0
unknown <kennylau1994@gmail.com>
2022-05-12 23:59:34 +0800

    added hosp api & cors

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
-FROM openjdk:11
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+FROM maven:3.6.0-jdk-11-slim AS build
+COPY src /home/app/src
+COPY pom.xml /home/app
+RUN mvn -f /home/app/pom.xml clean package
+
+FROM openjdk/openjdk-11-rhel7
+COPY --from=build /home/app/target/springbootapi-0.0.1-SNAPSHOT.jar /usr/local/lib/demo.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
+
+#FROM openjdk-11
+#ARG JAR_FILE=target/*.jar
+#COPY ${JAR_FILE} app.jar
+#ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/springbootapi/HelloController.java
+++ src/main/java/com/example/springbootapi/HelloController.java
@@ -1,14 +1,23 @@
 package com.example.springbootapi;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
 	
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Spring Boot";
+	}
+	
+	@RequestMapping("/hospitals")
+	public List<String> hospitals() {
+		return List.of("AHN", "TMH", "QMH", "PWH", "HHH", "TKO");
 	}
 
 }
